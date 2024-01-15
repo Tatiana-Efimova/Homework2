ccaca966bd1f7f46ce8d0e957a15b7dd524ae689
Tatiana <ef.tatiana@mail.ru>
2024-01-16 00:28:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int purchaseAmount = 10550; // рублей
+        int amount = 20; // рублей для одной бонусной мили
+
+        int m = purchaseAmount / amount; // миль
+
+        System.out.println(m);
+    }
+}
